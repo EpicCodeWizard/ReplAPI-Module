db0ae7b647238beda872852fe47a38881bac972f
Sarvesh Madullapalli <>
2021-05-10 17:38:21 +0000

    Added main file.

--- main.py
+++ main.py
@@ -1,266 +1,167 @@
-import requests, os, json, repltalk, asyncio, getpass, sys
-from bs4 import BeautifulSoup
+import requests
+import datetime
 
+refererurl = "https://replit.com/@EpicCodeWizard/ReplAPI-Module"
+referername = "ReplAPIit"
 
-class info(): 
-  def version():
-    print("VERSION: 0.0.6")
-
-  def owners():
-    print("OWNERS:\nMain Owner: JBYT27\nSide Owner(weird sidekick): darkdarcool")
-
-
-class assets():
-  async def replit_avatar_get(name = None):
-    if (name == None):
-      exit("ERROR: Please fill out the name parameter!")
-    else:
-      try:
-        client = repltalk.Client()
-        user = await client.get_user(name)
-        return user.avatar
-      except:
-        exit(f"ERROR: Cannot load {name}'s avatar!") 
-  async def replit_cycle_get(name = None):
-    if (name == None):
-      exit("ERROR: Please fill out the name parameter!")
-    else:
-      try:
-        client = repltalk.Client()
-        user = await client.get_user(name)
-        return user.cycles
-      except:
-        exit(f"ERROR: Cannot load {name}'s cycles!")
-  async def replit_name_get(name = None):
-    if (name == None):
-      exit("ERROR: Please fill out the name parameter!")
-    else:
-      try:
-        client = repltalk.Client()
-        user = await client.get_user(name)
-        return user.full_name
-      except:
-        exit(f"ERROR: Cannot load {name}'s name!")
-  async def replit_bio_get(name = None):
-    if (name == None):
-      exit("ERROR: Please fill out the name parameter!")
-    else:
-      try:
-        client = repltalk.Client()
-        user = await client.get_user(name)
-        return user.bio
-      except:
-        exit(f"ERROR: Cannot load {name}'s bio!")
-  async def replit_leaderboard_get():
-      #try:
-        client = repltalk.Client()
-        leaderboard = await client.get_leaderboard(limit=10)
-        return leaderboard#, sep="\n"
-      #except:
-       # exit(f"ERROR: Cannot load leaderboard!")
-
-def replit_user():
-    try:
-      owner = os.environ['REPL_OWNER']
-      return owner
-    except:
-      exit("ERROR: No such replit account exists!")
-def replit_avatar(name = None):
-  if (name == None):
-    exit("Please fill out the name parameter!")
+def replit_user_avatar(name):
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) {user: userByUsername(username: $username) {image}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    return queryjson['data']['user']['image']
   else:
-    try:
-      e = asyncio.run(assets.replit_avatar_get(f"{name}"))
-      return e
-    except:
-      exit(f"ERROR: Cannot find {name}'s avatar!")
-def replit_bio(name = None):
-  if (name == None):
-    exit("Please fill out the name parameter!")
+    return None
+
+def replit_user_cycle(name):
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) {user: userByUsername(username: $username) {karma}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    return queryjson['data']['user']['karma']
   else:
-    try:
-      e = asyncio.run(assets.replit_bio_get(f"{name}"))
-      return e
-    except:
-      exit(f"ERROR: Cannot find {name}'s bio!")
-def replit_name(name = None):
-  if (name == None):
-    exit("Please fill out the name parameter!")
+    return None
+
+def replit_user_name(name):
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) {user: userByUsername(username: $username) {fullName}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    return queryjson['data']['user']['fullName']
   else:
-    try:
-      e = asyncio.run(assets.replit_name_get(f"{name}"))
-      return e
-    except:
-      exit(f"ERROR: Cannot find {name}'s name!")
+    return None
 
-def replit_cycles(name = None):
-    if name == None:
-      exit("ERROR: You didn't fill out the name parameter!")
-    else:
-      try:
-        e = asyncio.run(assets.replit_cycle_get(f"{name}"))
-        return e
-      except:
-        exit("ERROR: Cannot find " + name + "'s cycles!")
-def replit_langs(name = None, extra = None):
-    sun = 0
-    if name == None:
-      exit("ERROR: You didn't fill out the name parameter!")
-    else:
-      if (extra == None):
-        pass
-      else:
-        if (extra == "all"):
-          sun = 1
-      try:
-        link = requests.get('https://replit.com/data/profiles/' +name )
-        data = link.json()
-        #print(data)
-        els = list(data.items())
-        all = els[-1]
-        if ("hacker" in all):
-          all = els[-2]
-        if ("isTeam" in all):
-          all = els[-1]
-        all = list(all)
-        stuff = all[1]
-        stuff = str(stuff)
-        stuff = stuff.replace("'", "")
-        stuff = stuff.replace("[", "")
-        stuff = stuff.replace("]", "")
-        stuff = stuff.replace(" ", "")
-        li = list(stuff.split(","))
-        if (sun == 1):
-          sun = 2
-          return li, sun
-        return (li)
-      except:
-        exit("ERROR: Cannot find " + name + "'s langs!")
-
-#start working here
-def replit_post(name = None):#latest post
-    if name == None:
-      exit("ERROR: You didn't fill out the name parameter!")
-    else:
-      #user = os.environ["REPL_OWNER"]
-      try:
-        post = requests.get("https://replit.com/@"+ name +"?tab=posts")#name
-        soup = BeautifulSoup(post.content, 'html.parser')
-        html1 = soup.find("div", {"class":"jsx-2329710370 board-post-list-item-post-title"}).get_text()
-        return html1
-        #b = a.replace('</div>','')
-        #return b
-      except:
-        exit("ERROR: Cannot find "+ name+"'s hottest post!")
-  
-def replit_posts(name = None):#all posts
-    if name == None:
-      exit("ERROR: You didn't fill out the name parameter!")
-    else:
-      try:
-        post = requests.get("https://replit.com/@"+ name +"?tab=posts")#name
-        soup = BeautifulSoup(post.content, 'html.parser')
-        html1 = soup.find_all("div", {"class":"jsx-2329710370 board-post-list-item-post-title"})#.get_text()
-        all_text = []
-        for i in html1:
-          all_text.append(i.get_text())
-        return '\n'.join(all_text)
-        
-      except:
-        exit("ERROR: Cannot find "+ name+"'s posts!")
-
-def replit_posts_len(name = None):#NEW FUNCTION
-  if name == None:
-    exit("ERROR: You didn't fill out the name parameter!")
+def replit_user_bio(name):
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) {user: userByUsername(username: $username) {bio}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    return queryjson['data']['user']['bio']
   else:
-    try:
-      post = requests.get("https://replit.com/@"+ name +"?tab=posts")#name
-      soup = BeautifulSoup(post.content, 'html.parser')
-      html1 = soup.find_all("div", {"class":"jsx-2329710370 board-post-list-item-post-title"})#.get_text()
-      num = 0
-      for i in html1:
-        num+=1
-      return str(num)
+    return None
 
-    except:
-      exit("ERROR: Cannot find "+name+"'s post length!")
-  
-def replit_comment(name = None):
-  if name == None:
-      exit("ERROR: You didn't fill out the name parameter!")
+def replit_user_hacker(name):
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) {user: userByUsername(username: $username) {isHacker}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    return queryjson['data']['user']['isHacker']
   else:
-      try:
-        post = requests.get("https://replit.com/@" + name + "?tab=comments")
-        soup = BeautifulSoup(post.content, 'html.parser')
-        for data in soup.find("p"):
-          return (data.get_text())
-          break # keep this here so it prints once
-      except:
-        exit("ERROR: Cannot find "+ name+"'s hottest comment!") 
-  
-def replit_comments(name = None):
-    if name == None:
-      exit("ERROR: You didn't fill out the name parameter!")
-    else:
-      try:
-        post = requests.get("https://replit.com/@" + name + "?tab=comments")
-        soup = BeautifulSoup(post.content, 'html.parser')
-        all_text = []
-        for data in soup.find_all("p"):
-          all_text.append(data.get_text())
-        return '\n'.join(all_text)
-      except:
-        exit("ERROR: Cannot find "+name+"'s comments!")
+    return None
 
-def replit_comments_len(name = None):#NEW FUNCTION
-  if name == None:
-    exit("ERROR: You didn't fill out the name parameter!")
+def replit_user_accountcreation(name):
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) {user: userByUsername(username: $username) {accountcreation}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    return datetime.datetime.strptime(queryjson['data']['user']['accountcreation'], '%Y-%m-%dT%H:%M:%S.%fZ')
   else:
+    return None
+
+def replit_user_organization(name):
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) {user: userByUsername(username: $username) {organization{name}}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    return queryjson['data']['user']['organization']['name']
+  else:
+    return None
+
+def replit_user_langs(name):
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) {user: userByUsername(username: $username) {languages{id}}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    listoflangs = []
+    for x in queryjson['data']['user']['languages']:
+      listoflangs.append(x['id'])
+    return listoflangs
+  else:
+    return None
+
+def replit_leaderboard():
+  information = requests.post('https://replit.com/graphql', json={'query': 'query PostsFeedLeaders($count: Int, $since: KarmaSince) {leaderboard(count: $count, since: $since) {items{username karma}}}', 'variables': {'count': 10}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()['data']['leaderboard']['items']
+  return information
+
+def replit_post(name):
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) { user: userByUsername(username: $username) {posts(order: "hot", count: 1) {pageInfo {nextCursor} items {title body voteCount}}}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
     try:
-      post = requests.get("https://replit.com/@" + name + "?tab=comments")
-      soup = BeautifulSoup(post.content, 'html.parser')
-      num=0
-      for data in soup.find_all("p"):
-        num+=1
-      return str(num)
+      return queryjson['data']['user']['posts']['items'][0]
     except:
-      exit("ERROR: Cannot find "+name+"'s comments length!")
-
-class talk():
-  def all_posts():#all posts, prob will not work lol 
-  # dumb idea, will break
-    pass
+      return None
+  else:
+    return None
   
-  def top_post():# top post 
-  # this one is okey
-    pass
-  
-  def top_posts():# few of the top posts
-  # this ones aight
-    pass
+def replit_posts(name):
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) { user: userByUsername(username: $username) {posts(order: "hot", count: 99999) {pageInfo {nextCursor} items {title body voteCount}}}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    return {"posts": queryjson['data']['user']['posts']['items']}
+  else:
+    return None
 
-  def new_post():# newest post
-  # this ones dumb but okey
-    pass
+def replit_posts_len(name):
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) { user: userByUsername(username: $username) {posts(order: "hot", count: 99999) {pageInfo {nextCursor} items {title body voteCount}}}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    return len(queryjson['data']['user']['posts']['items'])
+  else:
+    return None
   
-  def new_posts():
-    pass
-
-  def leaderboard():
+def replit_comment(name):
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) { user: userByUsername(username: $username) {comments(order: "hot", count: 1) {pageInfo {nextCursor} items {body voteCount}}}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
     try:
-      e = asyncio.run(assets.replit_leaderboard_get())
-      e = str(e)
-      e = e.replace('<', '')
-      e = e.replace('>', '')
-      e = e.replace('[', '')
-      e = e.replace(']', '')
-      e = list(e.split(', '))
-      # e = '\n'.join(e)
-      return e
+      return queryjson['data']['user']['comments']['items'][0]
     except:
-      exit("ERROR: Cannot load leaderboard!")
+      return None
+  else:
+    return None
+  
+def replit_comments(name):
+  comments = []
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) { user: userByUsername(username: $username) {comments(order: "hot", count: 99999) {pageInfo {nextCursor} items {body voteCount}}}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    comments += queryjson['data']['user']['comments']['items']
+    try:
+      after = queryjson['data']['user']['comments']['pageInfo']['nextCursor']
+      if after is None:
+        raise ValueError("Random error to break.")
+    except:
+      return {"comments": comments}
+    while True:
+      queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!, $after: String) { user: userByUsername(username: $username) {comments(order: "hot", count: 99999, after: $after) {pageInfo {nextCursor} items {body voteCount}}}}', 'variables': {'username': name, 'after': after}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+      comments += queryjson['data']['user']['comments']['items']
+      try:
+        after = queryjson['data']['user']['comments']['pageInfo']['nextCursor']
+        if after is None:
+          raise ValueError("Random error to break.")
+      except:
+        return {"comments": comments}
+  else:
+    return None
 
-repltalk = talk.leaderboard()
-os.system("clear")
-for i in range(10):
-  print(repltalk[i])
+def replit_comments_len(name):
+  comments = []
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) { user: userByUsername(username: $username) {comments(order: "hot", count: 99999) {pageInfo {nextCursor} items {body voteCount}}}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    comments += queryjson['data']['user']['comments']['items']
+    try:
+      after = queryjson['data']['user']['comments']['pageInfo']['nextCursor']
+      if after is None:
+        raise ValueError("Random error to break.")
+    except:
+      return len(comments)
+    while True:
+      queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!, $after: String) { user: userByUsername(username: $username) {comments(order: "hot", count: 99999, after: $after) {pageInfo {nextCursor} items {body voteCount}}}}', 'variables': {'username': name, 'after': after}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+      comments += queryjson['data']['user']['comments']['items']
+      try:
+        after = queryjson['data']['user']['comments']['pageInfo']['nextCursor']
+        if after is None:
+          raise ValueError("Random error to break.")
+      except:
+        return len(comments)
+  else:
+    return None
+
+def replit_repl(name):
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) { user: userByUsername(username: $username) {publicRepls(count: 1) {pageInfo {nextCursor} items {title description imageUrl runCount forkCount: publicForkCount}}}}', 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    try:
+      return queryjson['data']['user']['publicRepls']['items'][0]
+    except:
+      return None
+  else:
+    return None
+
+def replit_repls(name, replcount):
+  if replcount <= 0:
+    return None
+  queryjson = requests.post('https://replit.com/graphql', json={'query': 'query userByUsername($username: String!) { user: userByUsername(username: $username) {publicRepls(count: replcount) {pageInfo {nextCursor} items {title description imageUrl runCount forkCount: publicForkCount}}}}'.replace("replcount", str(replcount)), 'variables': {'username': name}}, headers={'referer': refererurl,'X-Requested-With': referername}).json()
+  if queryjson['data']['user'] is not None:
+    return {"repls": queryjson['data']['user']['publicRepls']['items']}
+  else:
+    return None
